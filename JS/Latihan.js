4867960ed4d106875ced6a8405d01f841cb6b36f
HanifMaziz7 <hanifmz7@gmail.com>
2021-05-05 10:00:18 +0700

    commit on vsCode 20210505

---
+++ JS/Latihan.js
@@ -0,0 +1,18 @@
+let myArray = ["Cokelat", 42.5, 22, true, "Programing"];
+console.log(myArray);
+// console.log(myArray[1]);
+// console.log(myArray[2]);
+// console.log(myArray[3]);
+// console.log(myArray[4]);
+// console.log(myArray[5]);
+// console.log("Panjang nilai myArray adalah" +myArray.length);
+
+//push command -- insert data pada array
+myArray.push('JavaScript');
+console.log(myArray);
+
+//mengeluarkan data dan elemen terahir array POP command
+myArray.pop();
+console.log(myArray);
+
+
